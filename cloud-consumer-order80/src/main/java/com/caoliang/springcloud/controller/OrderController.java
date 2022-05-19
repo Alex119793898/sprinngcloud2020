970127211638537f4f00a9b14bd2081c5f06eead
csl119793898 <119793898@qq.com>
2022-05-19 17:24:36 +0800

    12.RestTemplate的restTemplate.getForEntity(...)用法     * 返回 ResponseEntity对象，包含响应信息（如响应头、状态吗、响应体）

--- cloud-consumer-order80/src/main/java/com/caoliang/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/caoliang/springcloud/controller/OrderController.java
@@ -2,8 +2,11 @@ package com.caoliang.springcloud.controller;
 
 import com.caoliang.springcloud.entities.CommonResult;
 import com.caoliang.springcloud.entities.Payment;
-import com.oracle.tools.packager.Log;
+import com.sun.javafx.tools.packager.Log;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,9 +26,11 @@ public class OrderController {
     //指定服务提供者的服务名称cloud-payment-service
     public static final String PAYMENT_URL = "http://cloud-payment-service";
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderController.class);
+
     @GetMapping("/consumer/create")
     public CommonResult<Payment> create(Payment payment){
-        Log.info("sjfklsj");
+        LOGGER.info("sjfklsj");
         return restTemplate.postForObject(PAYMENT_URL + "/payment/create",payment,CommonResult.class);
     }
 
@@ -33,4 +38,18 @@ public class OrderController {
     public CommonResult<Payment> getPayment(@PathVariable("id") Long id){
         return restTemplate.getForObject(PAYMENT_URL + "/payment/getById/" + id, CommonResult.class);
     }
+
+    @GetMapping("/consumer/getForEntity/{id}")
+    public CommonResult<Payment> getPayment2(@PathVariable("id") Long id){
+
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL + "/payment/getById/" + id, CommonResult.class);
+
+        if(entity.getStatusCode().is2xxSuccessful()){
+            LOGGER.info(entity.getStatusCode()+"\t"+entity.getHeaders());
+            return entity.getBody();
+        }else {
+            return new CommonResult<>(444,"操作失败");
+        }
+    }
+
 }
