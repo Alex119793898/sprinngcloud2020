e761c6d4e8dc988cedcac3ce4ff662d83e0995e0
csl119793898 <119793898@qq.com>
2022-06-01 10:22:51 +0800

    15.服务提供端 8001 开启服务降级fallback方法：处理超时/运行时异常，避免重复不断地请求卡死在8001造成雪崩效应

--- cloud-provider-hystrix-payment8001/src/main/java/com/caoliang/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/caoliang/springcloud/service/PaymentService.java
@@ -1,5 +1,7 @@
 package com.caoliang.springcloud.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,6 +10,9 @@ public class PaymentService {
         return "线程池：" + Thread.currentThread().getName() + " paymentInfo_OK,id: " + id + "哈哈～";
     }
 
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeoutHandler", commandProperties = {
+            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value="5000")          //execution.isolation.thread.timeoutInMilliseconds当前线程的超时时间是3秒钟，但下面方法花费了5秒钟
+    })
     public String paymentInfo_TimeOut(Integer id){
 
         int timeNumber = 3000;
@@ -17,5 +22,10 @@ public class PaymentService {
             e.printStackTrace();
         }
         return "线程池：" + Thread.currentThread().getName() + " paymentInfo_TimeOut,id: " + id + "哈哈～" + " 耗时: " + timeNumber +" 毫秒";
+    }
+
+
+    public String paymentInfo_TimeoutHandler(Integer id){
+        return "线程池：" + Thread.currentThread().getName() + " 系统繁忙或者运行报错,请稍后再试，id: " + id + "哭～";
     }
 }
